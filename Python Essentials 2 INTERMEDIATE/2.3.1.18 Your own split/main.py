a0888c3fab37aabce56a3859d347645519f77dd1
Kamil <97406177+Bunnicz@users.noreply.github.com>
2022-05-05 21:22:12 +0200

    Add files via upload

---
+++ Python Essentials 2 INTERMEDIATE/2.3.1.18 Your own split/main.py
@@ -0,0 +1,21 @@
+﻿def mysplit(strng):
+    strng = strng.lstrip()
+    list = []
+    word = ""
+    for ch in strng:
+        if ch == " ":
+            if word != "":
+                list.append(word)
+            word = ""
+            continue
+        else:
+            word += ch
+    return list
+
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
+print(mysplit(" ab  ab  ab "))
